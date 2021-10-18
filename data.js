ab70ccdae56ad1cea8329aa1ed72db4bebb1eb48
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-10-17 20:50:24 -0600

    sample api

---
+++ data.js
@@ -0,0 +1,17 @@
+export const  projects = [ 
+    {
+        id: 1,
+        title:'this is a test title',
+        description: 'the project is test'
+    },
+    {
+        id: 2,
+        title: 'This is test title two',
+        description: 'Nothing works better'
+    },
+    {
+        id: 3,
+        title: 'Here is another one',
+        description:'Another one'
+    }
+]
--- pages/api/projects/index.js
+++ pages/api/projects/index.js
@@ -1,6 +1,35 @@
 /* eslint-disable import/no-anonymous-default-export */
+import { projects } from '../../../data'
+
+// export default function(req,res) { 
+// res.status(200).json({name: 'API TEST'})
+// }
 
 
-export default function(req,res) { 
-res.status(200).json({name: 'Jack Rigan'})
+
+// Dummy data to test 
+
+export default async (req,res) => {
+    const httpMethod = req.method;
+
+    // Request these properties
+    const {id, title, description} = req.body;
+
+    switch(httpMethod) {
+        case 'GET':
+            res.status(200).json(projects);
+            break
+            case "POST":
+                //Create new project record
+                res.status(200).json({
+                    title: title,
+                    id: id,
+                    description:description,
+                });
+                break;
+                default: 
+                res.setHeader('Allow', ['GET', 'POST'] );
+                // Returns a message if the status code is 405
+                res.status(405).end(`Method ${httpMethod} not welcome`)
+    } 
 }
