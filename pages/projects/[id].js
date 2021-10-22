c885e7efbc8c2499e5940151f713ff42e00e392c
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-10-21 20:07:21 -0600

    Updated projectpage styling

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,39 +1,82 @@
+import {Image, Box, Text, Flex} from '@chakra-ui/react'
+import Header from '../../src/components/Header/Header'
 
 export const getStaticPaths = async () => {
-     const res = await fetch ('https://my-json-server.typicode.com/JackXN/API-Rigan/projects')
-    const data = await res.json();
-    // console.log(data)
+  const res = await fetch(
+    "https://my-json-server.typicode.com/JackXN/API-Rigan/projects"
+  );
+  const data = await res.json();
+  // console.log(data)
 
-    const paths = data.map(item => {
-        return {
-            params: { id: item.id.toString() }
-        }
-    })
+  const paths = data.map((item) => {
     return {
-        paths,
-        fallback: false
-    }
-    }
-
+      params: { id: item.id.toString() },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
 
 export const getStaticProps = async (context) => {
-    const id = context.params.id;
-    const res = await fetch(`https://my-json-server.typicode.com/JackXN/API-Rigan/projects/${id}`)
-    const data = await res.json();
+  const id = context.params.id;
+  const res = await fetch(
+    `https://my-json-server.typicode.com/JackXN/API-Rigan/projects/${id}`
+  );
+  const data = await res.json();
 
-    return {
-        props: { project: data }
+  return {
+    props: { project: data },
+  };
+};
+
+const ProjectDetail = ({ project }) => {
+  return(
+      <>
+      <Header/>
+   <Box>
+<Flex sx={styles.topBox}>
+    <Text as='h1' sx={styles.title}>{project.title}</Text>
+    <Text as='h3' sx={styles.description}>{project.description}</Text>
+    
+</Flex>
+   </Box>
+
+   </>
+  )
+};
+
+
+const styles = {
+    topBox: {
+        padding: '20px',
+        background: '#1B1F23',
+        color: 'white',
+        flexDirection: 'column',
+        alignItems:'center',
+        justifyContent: 'center',
+        textAlign:'center',
+        pt:'50px',
+        pb: '50px',
+        height: '100vh'
+    },
+    title: {
+    fontFamily: "Bebas Neue, cursive",
+    fontSize: '50px', 
+    fontWeight: '400',
+    lineHeight: '60px',
+    margin: '40px'
+
+    },
+    description: {
+
+    },
+    subDescription: {
+ color: 'black',
+ mt: '20px',
+
     }
 }
-
-
-
-
-const ProjectDetail = ({project}) => {
-    return (
-  <h1>{project.title}</h1>
-    )
-}
-
 
 export default ProjectDetail;
