785397fc8ee4343a426b5d73b27f9bffca982c02
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-11-06 23:17:39 -0600

    Updated buttons for the project banners

--- src/components/ProjectBanner.js
+++ src/components/ProjectBanner.js
@@ -1,7 +1,7 @@
 import { Box, Text } from "@chakra-ui/layout";
 import { Image } from "@chakra-ui/react";
 import Link from 'next/link';
-import { UnorderedList, ListItem } from "@chakra-ui/react";
+import { UnorderedList, ListItem,Button } from "@chakra-ui/react";
 
 
 const ProjectBanner = ({project}) => {
@@ -11,18 +11,44 @@ const ProjectBanner = ({project}) => {
 <Image src={project.imgOne} alt={project.title} sx={styles.img}/>
 <Box sx={styles.links}>
 <UnorderedList>
-  <ListItem sx={styles.linkItem}>
-    <Link href='#'>View Live Project</Link>
-  </ListItem>
-  <ListItem  sx={styles.linkItem}>
-    <Link href='#'>
-      Read The Documentation
-    </Link>
-  </ListItem>
+  <ListItem>
+  <Button
+            sx={styles.buttons.primary}
+            as="button"
+            _hover={{
+              boxShadow: "inset -3.5em 0 0 0 var(--hover)",
+              boxShadow: "inset 3.5em 0 0 0 var(--hover)",
+            }}
+            focus={{
+              boxShadow: "inset -3.5em 0 0 0 var(--hover)",
+              boxShadow: "inset 3.5em 0 0 0 var(--hover)",
+            }}
+            color="orange"
+            transition="0.25s"
+          >
+            Learn More
+          </Button>
+          </ListItem>
+          <ListItem>
+  <Button
+            sx={styles.buttons.primary}
+            as="button"
+            _hover={{
+              boxShadow: "inset -3.5em 0 0 0 var(--hover)",
+              boxShadow: "inset 3.5em 0 0 0 var(--hover)",
+            }}
+            focus={{
+              boxShadow: "inset -3.5em 0 0 0 var(--hover)",
+              boxShadow: "inset 3.5em 0 0 0 var(--hover)",
+            }}
+            color="orange"
+            transition="0.25s"
+          >
+            Learn More
+          </Button>
+          </ListItem>
 </UnorderedList>
-
 </Box>
-
    </Box>
     </>
   );
@@ -44,7 +70,7 @@ linkItem: {
   color: 'white',
   margin: ['10px', null, null, '60px'],
   listStyle: 'none',
-  // width: '500px',
+  // width: '150px',
   fontSize: ['25px', '25px', '25px', '35px'],
   fontFamily: "Bebas Neue, cursive",
   textTransform: 'uppercase',
@@ -52,12 +78,70 @@ linkItem: {
   background: '#F06D69',
   borderRadius:['20px', null, null,null, '30px'],
   padding: ['10px', null , null, '30px'],
+  boxShadow: '5px 5px 5px  black',
+  border: 'none',
+
+  '&:hover': {
+    transition: '1s',
+    boxShadow:'0px 0px 0px 0px ',
+    cursor: 'pointer'
+  }
 },
 img: {
 height: ['400px', null, null, '500px'],
 width: ['400px', null, null, '500px'],
 mb: ['100px'],
-}
+},
+  buttons: {
+    container: {
+      align: "center",
+      justify: "space-between",
+      padding: "20px",
+      mt: [null, null, null, "20px"],
+    },
+    primary: {
+      padding: "25px",
+      margin: "10px",
+      ml: "-20px",
+      background: "linear-gradient(to right, #040405 50%, #AD0541 50%)",
+      color: "white",
+      transition: "all .5s ease-out",
+      borderRadius: "none",
+      fontSize: ["15px", null, "20px", "25px"],
+      backgroundPosition: " right bottom",
+      backgroundSize: "200% 100%",
+      transition: "0.25s",
+      "&:focus": {
+        inset: "-3.5rem 0 0 0 var(--hover)",
+        inset: "3.5rem 0 0 0 var(--hover)",
+      },
+      "&:hover": {
+        backgroundPosition: "left bottom",
+        color: "white",
+      },
+    },
+    secondary: {
+      padding: "25px",
+      margin: "10px",
+      ml: "20px",
+      background: "linear-gradient(to right, #040405 50%, #191A1A 50%)",
+      color: "white",
+      transition: "all .5s ease-out",
+      borderRadius: "none",
+      fontSize: ["15px", null, "20px", "25px"],
+      backgroundPosition: " right bottom",
+      backgroundSize: "200% 100%",
+      transition: "0.25s",
+      "&:focus": {
+        inset: "-3.5rem 0 0 0 var(--hover)",
+        inset: "3.5rem 0 0 0 var(--hover)",
+      },
+      "&:hover": {
+        backgroundPosition: "left bottom",
+        color: "white",
+      },
+    },
+  },
 };
 
 export default ProjectBanner;
